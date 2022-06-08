81d8fcaf1bd3e742f38e830da2f82f7848eff360
Boram No <boramm316@gmail.com>
2022-06-08 11:53:35 +0900

    Add Styled-Components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import styled, {keyframes} from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+`
+
+const animation = keyframes`
+  0% {
+    transform: rotate(0deg);
+    border-radius: 0px;
+  }
+  50% {
+    border-radius: 100px;
+  }
+  100% {
+    transform: rotate(360deg);
+    border-radius: 0px;
+  }
+`
+
+const Emoji = styled.span`
+  font-size: 36px;
+`
+
+const Box = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 200px;
+  height: 200px;
+  background-color: tomato;
+  animation: ${animation} 1s linear infinite;
+  ${Emoji} {
+    &:hover {
+      font-size: 98px;
+    }
+    &:active {
+      opacity: 0;
+    }
+  }
+`
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Wrapper>
+      <Box>
+        <Emoji as="div">🤩</Emoji>
+      </Box>
+    </Wrapper>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// ReactDOM.render를 사용하면 브라우저 에러가 발생하는데 이는 ReactDOM.render를 리액트 v18부터 사용하지 않기 때문에 나타나는 에러이다.
